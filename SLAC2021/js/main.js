8b2a54f0baf4b090e8d90fd602c75e392f6d8d8a
Nipuni Wijeratne <nipuniwijeratne78@gmail.com>
2021-04-07 13:47:46 +0530

    Project openining

---
+++ SLAC2021/js/main.js
@@ -0,0 +1,152 @@
+$(function() {
+
+  var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	var siteMenuClone = function() {
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+});
+
+
+//Affiliations Slider
+$(document).ready(function () {
+	$('.customer-logos').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: false,
+		dots: false,
+		pauseOnHover: true,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 4
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 3
+			}
+		}]
+	});
+});
+//END Affiliations Slider
+
+
+//Video Testimonials here
+$(".video-play").on('click', function (e) {
+	e.preventDefault();
+	var vidWrap = $(this).parent(),
+		iframe = vidWrap.find('.video iframe'),
+		iframeSrc = iframe.attr('src'),
+		iframePlay = iframeSrc += "?autoplay=1";
+	vidWrap.children('.video-thumbnail').fadeOut();
+	vidWrap.children('.video-play').fadeOut();
+	vidWrap.find('.video iframe').attr('src', iframePlay);
+});
+//Video Testominials ENDS here
+
+
+
+//Back to Top button
+$(document).ready(function () {
+	$(function () {
+		$(document).on('scroll', function () {
+			if ($(window).scrollTop() > 100) {
+				$('.back-to-top').fadeIn();
+			} else {
+				$('.back-to-top').fadeOut();
+			}
+		});
+		$('.back-to-top').on('click', scrollToTop);
+	});
+
+	function scrollToTop() {
+		verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+		element = $('body');
+		offset = element.offset();
+		offsetTop = offset.top;
+		$('html, body').animate({ scrollTop: offsetTop }, 500, 'linear');
+	}
+});
+//END Back to Top button

f02558029fb1594cbeb2293672a96eb25c66f81a
Mickey <kasun.fdo@gmail.com>
2021-05-14 15:47:25 +0530

    code clean up review and fixes

--- SLAC2021/js/main.js
+++ SLAC2021/js/main.js
@@ -90,10 +90,10 @@ $(function() {
 //Affiliations Slider
 $(document).ready(function () {
 	$('.customer-logos').slick({
-		slidesToShow: 6,
+		slidesToShow: 4,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 1500,
+		autoplaySpeed: 2000,
 		arrows: false,
 		dots: false,
 		pauseOnHover: true,
@@ -105,7 +105,7 @@ $(document).ready(function () {
 		}, {
 			breakpoint: 520,
 			settings: {
-				slidesToShow: 3
+				slidesToShow: 2
 			}
 		}]
 	});
